ba8b236736fe5132464c0da9d1aead97c1333559
Michael Bryan <michaelfbryan@gmail.com>
2016-04-01 03:07:53 +0800

    Set up argparse for the scraper

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,21 +1,35 @@
+import argparse
+import sys
 import spider_board
 
 
-def main():
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('username', help='Your username')
+    parser.add_argument('password', help='Your password')
+    parser.add_argument('-s', '--sequential', dest='sequential', 
+            action='store_true', help='Run sequentially')
+    parser.add_argument('-t', '--threads', dest='threads', type=int, default=8,
+            help='Number of threads to use (default: 8)')
+
+    args = parser.parse_args(argv)
+
+    username = args.username
+    password = args.password
+
+    if args.sequential:
+        run_sequentially = True
+    else:
+        run_sequentially = False
+
     username = '17052933'
     password = 'Popcorn37'
 
-    bob = spider_board.Browser(username, password, threads=1)
+    bob = spider_board.Browser(username, password, seq=run_sequentially)
     bob.start()
 
-    # bob.login()
-    # bob.get_units()
-
-    # a_unit = bob.units[0]
-    # bob.find_documents(a_unit)
-
-    # print(a_unit.documents.qsize())
+    print(bob.documents.qsize())
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
